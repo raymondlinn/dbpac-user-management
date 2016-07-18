5e70f146dff176a4445cc719fffb7f179e11da48
Raymond Linn <rlinn@alpine-la.com>
2016-07-18 13:29:26 -0700

    added class-dbpac-user-manager.php

---
+++ class-dbpac-user-manager.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Plugin Name: DBPAC User Manager
+  Plugin URI:
+  Description: User management functionality for DBPAC application.
+  Author: Raymond Linn
+  Version: 1.0
+  Author URI: http://raymondlinn.com/
+*/
+
+class DBPAC_User_Manager {
+	public function __construct() {
+
+	}
+}
+
+$dbpac_user_manager = new DBPAC_User_Manager();
