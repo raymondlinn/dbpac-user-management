43d1d4274bd6448a129cb44e19efabaf82a76fbc
Raymond Linn <rlinn@alpine-la.com>
2016-07-18 13:21:16 -0700

    first commit

---
+++ dbpac-user-manager.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: DBPAC User Manager
+  Plugin URI:
+  Description: User management functionality for DBPAC application.
+  Author: Raymond Linn
+  Version: 1.0
+  Author URI: http://raymondlinn.com/
+*/
