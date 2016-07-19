c935a049fe32339cae4b1ead880b839327aced97
raymondlinn <raymondlinn@users.noreply.github.com>
2016-07-18 20:06:15 -0700

    Delete dbpac-user-manager.php

--- dbpac-user-manager.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-  Plugin Name: DBPAC User Manager
-  Plugin URI:
-  Description: User management functionality for DBPAC application.
-  Author: Raymond Linn
-  Version: 1.0
-  Author URI: http://raymondlinn.com/
-*/
